51778f4443d61b116cdaa9d6b1b818e2198fd097
Abel Muriithi <abelmuriithi04@gmail.com>
2022-11-27 18:49:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+    
+    let mondayWork = function(willDo="go to the office"){
+     return `This Monday, I will ${willDo}.` 
+    }
+    mondayWork()
+    
+    function wrapAdjective (style="*") {
+      return function(stringOne="special"){
+        return `You are ${style}${stringOne}${style}!`
+      }
+    }
